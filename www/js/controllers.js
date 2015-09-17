4034f0ff85d1e906c46a595665ffdfdf358f27af
Aleksandra Desmurs-Linczewska <olala@MacBook-Pro-Lukasz.local>
2015-09-17 14:42:35 +0200

    corrected name of the app

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('gooeyApp.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
--- www/js/directives.js
+++ www/js/directives.js
@@ -1,4 +1,4 @@
-angular.module('starter.directives', [])
+angular.module('gooeyApp.directives', [])
 
 .directive('gooeyTabs', [
   '$ionicTabsDelegate',
